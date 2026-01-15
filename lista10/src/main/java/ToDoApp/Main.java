9b8f246b37b33d93bc23af4754489dcf0a873bb7
Jakub <jakub_dluzak@wp.pl>
2026-01-15 10:43:27 +0100

    Delete lista10 directory

--- lista10/src/main/java/ToDoApp/Main.java
+++
@@ -1,9 +0,0 @@
-package ToDoApp;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(MainApp.class, args);
-    }
-}
